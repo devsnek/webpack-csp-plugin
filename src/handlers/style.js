8302c4a65c87a2ff0112ffe2ef350146d6c3b165
Gus Caplan <me@gus.host>
2017-06-30 06:45:37 -0500

    better css support

--- src/handlers/style.js
+++ src/handlers/style.js
@@ -2,20 +2,32 @@ const crypto = require('crypto');
 const { URL } = require('url');
 const CSP = require('csp-header');
 
-module.exports = function({ node, csp, options }) {
-  const href = node.attrs.find(a => a.name === 'href');
-  if (href) {
-    const absolute = /^https?:\/\//i.test(href.value);
+module.exports = function({ src, node, csp, options }) {
+  if (node) {
+    const href = node.attrs.find(a => a.name === 'href');
+    if (href) {
+      addHref(href.value);
+    } else {
+      let style = node.childNodes.find(n => n.nodeName === '#text');
+      if (!style) return;
+      const hash = crypto.createHash(options.hashType).update(style.value).digest('hex');
+      csp.push(`'${options.hashType}-${hash}'`);
+    }
+  } else if (src) {
+    const re = /@import (?:url\()?(.+?)\)?;/g;
+    let match;
+    while ((match = re.exec(src)) !== null) {
+      addHref(match[1].replace(/("|')/g, ''));
+    }
+  }
+
+  function addHref(href) {
+    const absolute = /^https?:\/\//i.test(href);
     if (absolute) {
-      const origin = new URL(href.value).host;
+      const origin = new URL(href).host;
       if (!csp.includes(origin)) csp.push(origin);
     } else if (!csp.includes(CSP.SELF)) {
       csp.push(CSP.SELF);
     }
-  } else {
-    let style = node.childNodes.find(n => n.nodeName === '#text');
-    if (!style) return;
-    const hash = crypto.createHash(options.hashType).update(style.value).digest('hex');
-    csp.push(`'${options.hashType}-${hash}'`);
   }
 };
--- src/index.js
+++ src/index.js
@@ -18,31 +18,12 @@ class WebpackCspPlugin {
       'worker-src': options.workerSRC || [],
     };
   }
+
   apply(compiler) {
     compiler.plugin('emit', (compilation, callback) => {
       for (const [name, value] of Object.entries(compilation.assets)) {
-        if (!name.endsWith('.html')) continue;
-        const ast = parse5.parseFragment(value.source());
-        (function walk(obj) {
-          if (!obj.childNodes) return;
-          for (const node of obj.childNodes) {
-            switch (node.nodeName) {
-              case 'script':
-                handlers.script({ node, csp: this.csp['script-src'], options: this.options });
-                handlers.worker({ node, csp: this.csp['worker-src'], options: this.options });
-                break;
-              case 'link':
-                handlers.style({ node, csp: this.csp['style-src'], options: this.options });
-                break;
-              case 'img':
-                handlers.image({ node, csp: this.csp['img-src'], options: this.options });
-                break;
-              default:
-                break;
-            }
-            walk(node);
-          }
-        }.bind(this)(ast));
+        if (name.endsWith('.html')) this.handleHTML(value);
+        else if (name.endsWith('.css')) this.handleCSS(value);
       }
 
       const output = this.options.output;
@@ -62,6 +43,34 @@ class WebpackCspPlugin {
 
       callback();
     });
+  }
+
+  handleHTML(value) {
+    const ast = parse5.parseFragment(value.source());
+    (function walk(obj) {
+      if (!obj.childNodes) return;
+      for (const node of obj.childNodes) {
+        switch (node.nodeName) {
+          case 'script':
+            handlers.script({ node, csp: this.csp['script-src'], options: this.options });
+            handlers.worker({ node, csp: this.csp['worker-src'], options: this.options });
+            break;
+          case 'link':
+            handlers.style({ node, csp: this.csp['style-src'], options: this.options });
+            break;
+          case 'img':
+            handlers.image({ node, csp: this.csp['img-src'], options: this.options });
+            break;
+          default:
+            break;
+        }
+        walk(node);
+      }
+    }.bind(this)(ast));
+  }
+
+  handleCSS(value) {
+    handlers.style({ src: value.source(), csp: this.csp['style-src'], options: this.options });
   }
 }
 
